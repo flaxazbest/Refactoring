3bd3dca7758cc1e11de909c183e8d9e35e4a0e5e
powerex <flaxazbest@gmail.com>
2018-10-10 10:38:39 +0300

    Source program

---
+++ src/main/java/videostore/Client.java
@@ -0,0 +1,22 @@
+package videostore;
+
+public class Client {
+
+    public static void main(String[] args) {
+        Movie movie1 = new Movie("Ant man and Wisp", Movie.NEW_RELEASE);
+        Movie movie2 = new Movie("Toy story", Movie.CHILDRENS);
+        Movie movie3 = new Movie("X-man: origin wolverine", Movie.REGULAR);
+
+        Rental rental1 = new Rental(movie1, 5);
+        Rental rental2 = new Rental(movie2, 2);
+        Rental rental3 = new Rental(movie3, 4);
+
+        Customer client1 = new Customer("Vidos");
+        client1.addRental(rental1);
+        client1.addRental(rental2);
+        client1.addRental(rental3);
+
+        System.out.println(client1.statement());
+    }
+
+}
---
+++ src/main/java/videostore/Customer.java
@@ -0,0 +1,66 @@
+package videostore;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Customer {
+
+    private String name;
+    private List<Rental> rentals = new LinkedList<Rental>();
+
+    public Customer(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void addRental(Rental arg) {
+        rentals.add(arg);
+    }
+
+    public String statement() {
+        double totalAmount = 0;
+        int frequentRenterPoints = 0;
+        Iterator<Rental> rents = rentals.iterator();
+        StringBuilder result = new StringBuilder();
+        result.append("Звіт аренди для ").append(getName()).append("\n");
+        while (rents.hasNext()) {
+            double thisAmount = 0;
+            Rental each = (Rental)rents.next();
+            switch (each.getMovie().getPriceCode()) {
+                case Movie.REGULAR:
+                    thisAmount += 2;
+                    if (each.getDaysRented() > 2)
+                        thisAmount += (each.getDaysRented() - 2) * 1.5;
+                    break;
+                case Movie.NEW_RELEASE:
+                    thisAmount += each.getDaysRented() * 3;
+                    break;
+                case Movie.CHILDRENS:
+                    thisAmount += 1.5;
+                    if (each.getDaysRented() > 3)
+                        thisAmount += (each.getDaysRented() - 3) * 1.5;
+                    break;
+            }
+
+            frequentRenterPoints++;
+            if (each.getMovie().getPriceCode() == Movie.NEW_RELEASE && each.getDaysRented() > 1)
+                frequentRenterPoints++;
+
+            result.append("\t").append(each.getMovie().getTitle())
+                   .append("\t").append(String.valueOf(thisAmount))
+                   .append("\n");
+
+            totalAmount += thisAmount;
+        }
+
+        result.append("Сума заборгованості складає ").append(String.valueOf(totalAmount)).append("\n")
+              .append("Ви заробили ").append(String.valueOf(frequentRenterPoints))
+              .append(" балів за активність");
+
+        return result.toString();
+    }
+}
