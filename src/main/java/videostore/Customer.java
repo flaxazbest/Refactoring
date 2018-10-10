097d369153ecd6938ab18292e544410b17dcaff2
powerex <flaxazbest@gmail.com>
2018-10-10 13:37:53 +0300

    - Replace temp with Query

--- src/main/java/videostore/Customer.java
+++ src/main/java/videostore/Customer.java
@@ -9,44 +9,48 @@ public class Customer {
     private String name;
     private List<Rental> rentals = new LinkedList<Rental>();
 
-    public Customer(String name) {
+    Customer(String name) {
         this.name = name;
     }
 
-    public String getName() {
+    String getName() {
         return name;
     }
 
-    public void addRental(Rental arg) {
+    void addRental(Rental arg) {
         rentals.add(arg);
     }
 
-    public String statement() {
-        double totalAmount = 0;
-        int frequentRenterPoints = 0;
+    String statement() {
         Iterator<Rental> rents = rentals.iterator();
         StringBuilder result = new StringBuilder();
         result.append("Звіт аренди для ").append(getName()).append("\n");
         while (rents.hasNext()) {
-            Rental each = (Rental)rents.next();
-
-            frequentRenterPoints += each.getFrequentRenterPoints();
+            Rental each = rents.next();
 
             result.append("\t").append(each.getMovie().getTitle())
                    .append("\t").append(String.valueOf(each.getCharge()))
                    .append("\n");
-
-            totalAmount += each.getCharge();
         }
 
-        result.append("Сума заборгованості складає ").append(String.valueOf(totalAmount)).append("\n")
-              .append("Ви заробили ").append(String.valueOf(frequentRenterPoints))
+        result.append("Сума заборгованості складає ").append(String.valueOf(getTotalCharge())).append("\n")
+              .append("Ви заробили ").append(String.valueOf(getTotalFrequentRenterPoints()))
               .append(" балів за активність");
 
         return result.toString();
     }
 
-    private double amountFor(Rental rental) {
-        return rental.getCharge();
+    private double getTotalCharge() {
+        double result = 0;
+        for (Rental r: rentals)
+            result += r.getCharge();
+        return result;
+    }
+
+    private int getTotalFrequentRenterPoints() {
+        int result = 0;
+        for (Rental r: rentals)
+            result += r.getFrequentRenterPoints();
+        return result;
     }
 }
